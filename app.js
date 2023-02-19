8ba3f42444b75c275e16153a68be51e34edea119
Xavier Maillou <axrevi@gmail.com>
2023-02-19 18:43:41 +0100

    sql changes according to user position availability

--- app.js
+++ app.js
@@ -11,25 +11,36 @@ app
   .use(express.json())
 
 app.get('/places/:latitude?/:longitude?/:distance?/:search?', (req, res) => {
-  const currentLatitude = Number(req.params.latitude) || 0
-  const currentLongitude = Number(req.params.longitude) || 0
+  const currentLatitude = Number(req.params.latitude) || null
+  const currentLongitude = Number(req.params.longitude) || null
   const requiredDistance = Number(req.params.distance) || 999999999999
   const searchQuery = req.params.search || ''
 
-  let sql = `SELECT *,
+  let sql = (currentLatitude && currentLongitude) ?
+  `SELECT *,
     SQRT(POW(111320 * (ABS(latitude - ?)), 2) + POW(111320 * (ABS(longitude - ?)), 2)) AS distance
     FROM places
     WHERE name like concat('%', ?, '%')
     HAVING distance < ?
     ORDER BY distance
   `
+  :
+  `SELECT *
+    FROM places
+    WHERE name like concat('%', ?, '%')
+  `
 
   db.query(sql,
+    (currentLatitude && currentLongitude) ?
     [
       currentLatitude,
       currentLongitude,
       searchQuery,
       requiredDistance,
+    ]
+    :
+    [
+      searchQuery,
     ], (err, queryResult) => {
       if (err) throw err
       res.send(queryResult)
