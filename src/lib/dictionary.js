d22b8ba3ce6adb18155ea412345ddacdec9029d9
Xavier Maillou <axrevi@gmail.com>
2023-04-06 21:23:58 +0200

    dictionary fix

--- src/lib/dictionary.js
+++ src/lib/dictionary.js
@@ -3,47 +3,47 @@ module.exports = {
         'cafe': {
             column: 'type',
             operation: 'eq',
-            value: '1'
+            value: 1
         },
         'quiet': {
             column: 'quiet',
             operation: 'gt',
-            value: '2'
+            value: 1
         },
         'calm': {
             column: 'quiet',
             operation: 'gt',
-            value: '2'
+            value: 1
         },
         'noisy': {
             column: 'quiet',
             operation: 'lt',
-            value: '3'
+            value: 2
         },
         'busy': {
             column: 'quiet',
             operation: 'lt',
-            value: '3'
+            value: 2
         },
         'wifi': {
             column: 'wifi',
             operation: 'gt',
-            value: '2'
+            value: 2
         },
         'connection': {
             column: 'wifi',
             operation: 'gt',
-            value: '2'
+            value: 2
         },
         'outlet': {
             column: 'outlet',
             operation: 'gt',
-            value: '0'
+            value: 0
         },
         'plug': {
             column: 'outlet',
             operation: 'gt',
-            value: '0'
+            value: 0
         },
     }
 }
