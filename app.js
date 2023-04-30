0eea3055bb996c44ec5acf0e0d64b77b35d1938b
Xavier Maillou <axrevi@gmail.com>
2023-04-30 23:40:50 +0200

    port updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 9000
 
 const auth = require('./src/firebase')
 
